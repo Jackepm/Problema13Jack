13689bebc32402b7812e19b07e3ea5bb3607089b
Jackepm <jackpaalomino@gmail.com>
2024-09-26 22:25:12 -0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -43,7 +43,7 @@ class Program
             Console.WriteLine($"Sueldo final de {nombre}: {sueldoFinal:C2}");
         }
 
-        Console.WriteLine($"\nMonto total de sueldos: {totalSueldos}");
+        Console.WriteLine($"\nMonto total de sueldos: {totalSueldos:C2}");
         Console.WriteLine($"Empleados que ganan entre 2500 y 3500: {empleados1}");
         Console.WriteLine($"Empleados que ganan menos de 1000 o más de 4000: {empleados2}");
     }
